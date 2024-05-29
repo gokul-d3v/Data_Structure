e2f8ee30c45febc49a9d15f65abcf51bd32f0344
gokul-d3v <richugokulkm@gmail.com>
2024-05-29 20:40:31 +0530

    added more methods

--- linked_list.js
+++ linked_list.js
@@ -45,6 +45,43 @@ class LinkedList {
       curr = curr.next;
     }
   }
+
+  //   insert between the values
+  insert(target, value) {
+    const node = new Node(value);
+    if (!this.head) {
+      console.log("the linked lis empty please add some values");
+      return;
+    }
+    let curr = this.head;
+    let prev;
+    while (curr != null && curr.data != target) {
+      prev = curr;
+      curr = curr.next;
+    }
+    prev = curr.next;
+    curr.next = node;
+    node.next = prev;
+  }
+
+  //   deletion of a node in linked list
+
+  delete(target) {
+    if (!this.head) {
+      return;
+    }
+    let curr = this.head;
+    let prev;
+    while (curr != null && curr.data != target) {
+      prev = curr;
+      curr = curr.next;
+    }
+    if (curr == this.tail || curr == null) {
+      return;
+    }
+    prev.next = curr.next;
+  }
+  //   largest and second largest in LinkedList
 }
 
 const l = new LinkedList();
@@ -53,4 +90,6 @@ l.prepend(9);
 l.append(20);
 l.append(30);
 l.prepend(8);
+l.insert(10, 25);
+l.delete(25);
 l.display();
